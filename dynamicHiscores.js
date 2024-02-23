a3370e0fbecdafdcc843abbef3d502b35439a60d
Furkan Coskun <furkdurk7@aol.com>
2024-02-22 20:52:25 -0500

    stylingNewsBox

---
+++ dynamicHiscores.js
@@ -0,0 +1,9 @@
+// const infoUrl = 'https://secure.runescape.com/m=itemdb_rs/api/info.json';
+
+
+// fetch(infoUrl).then(response => {
+//     if (!response.ok) {
+//         console.log("Http access Error")
+//     }
+//     return response.json()
+// })
