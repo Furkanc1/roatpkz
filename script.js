c63ba79e262e548d97a44b50530dece6920009bf
Furkan Coskun <furkdurk7@aol.com>
2024-02-22 22:08:20 -0500

    Touched up HTML+CSS + added forms and began form logic

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
-let players = localStorage.getItem(`players`)
-  ? JSON.parse(localStorage.getItem(`players`))
+let storedPlayers = localStorage.getItem(`players`);
+let players = storedPlayers
+  ? JSON.parse(storedPlayers)
   : Array(531).fill(null);
+
+// Every User in out database
+let storedUsers = localStorage.getItem(`users`);
+let users = storedUsers ? JSON.parse(storedUsers) : [];
+
+let storedUser = localStorage.getItem(`user`);
+// Currently Logged In User
+let user = storedUser ? JSON.parse(storedUser) : [];
+
+const signUpLogic = (data) => {
+    // If the email does not exist, sign the user up by storing them in localstorage
+    // If the email does exist => sign in logic
+    console.log(`User is trying to sign up`, data);
+}
+
+const signInLogic = (data) => {
+    // Check if the email exists in ou localstorage, if it does, now check their password, if both match, log user in with localstorage
+    console.log(`User is trying to sign in`, data);
+}
+
+let forms = document.querySelectorAll(`form`);
+if (forms && forms.length > 0) {
+    forms.forEach(form => {
+        form.addEventListener(`submit`, onFormSubmitEvent => {
+            onFormSubmitEvent.preventDefault();
+
+            let formThatWasSubmitted = onFormSubmitEvent.target;
+
+            let emailField = formThatWasSubmitted?.email;
+            let passwordField = formThatWasSubmitted?.password;
+
+            // let { name: nameField, password: passwordField } = formThatWasSubmitted;
+
+            let dataToPassToFunctions = {
+                formThatWasSubmitted,
+                email: emailField?.value,
+                password: passwordField?.value,
+            }
+
+            if (formThatWasSubmitted.classList.contains(`signUpForm`)) {
+                signUpLogic(dataToPassToFunctions);
+            } else if (formThatWasSubmitted.classList.contains(`signInForm`)) {
+                signInLogic(dataToPassToFunctions);
+            }
+        })
+    })
+}
 
 const setHeaderDynamically = () => {
   const header = $(`
-        <header>
+        <header class="insertedDynamicallyFromJS">
             <div class="topbar flexRow">
                 <div class="flexRow leftCol">
                     <div class="dropdown fontColorMain">More On Roat PKZ</div>
